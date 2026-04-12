21926d342800cbd9a9cae732f6f78659aba4632e
ricecake1111113 <107082467+ricecake1111113@users.noreply.github.com>
2026-04-11 21:56:27 -0600

    Fix OMORI web frame scaling to max-fit with preserved aspect ratio

--- file/omori/www_playtest_b30ab4fd/www/js/main.js
+++ file/omori/www_playtest_b30ab4fd/www/js/main.js
@@ -4,6 +4,29 @@
 
 PluginManager.setup($plugins);
 
+function applyResponsiveFrameScaling() {
+    if (!window.Graphics) {
+        return;
+    }
+
+    Graphics._stretchEnabled = true;
+    Graphics._updateRealScale = function() {
+        var viewportWidth = window.innerWidth || document.documentElement.clientWidth || this._width;
+        var viewportHeight = window.innerHeight || document.documentElement.clientHeight || this._height;
+        var horizontalScale = viewportWidth / this._width;
+        var verticalScale = viewportHeight / this._height;
+
+        if (!isFinite(horizontalScale) || horizontalScale <= 0) horizontalScale = 1;
+        if (!isFinite(verticalScale) || verticalScale <= 0) verticalScale = 1;
+
+        this._realScale = Math.min(horizontalScale, verticalScale);
+    };
+}
+
+applyResponsiveFrameScaling();
+
 window.onload = function() {
+    applyResponsiveFrameScaling();
     SceneManager.run(Scene_Boot);
 };
+
